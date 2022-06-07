27c4397311364d48f6a04053aae78d16eef58f25
zzrs123 <2296704779@qq.com>
2022-06-07 11:00:43 +0800

    test2 finish

--- information-code-py/worldcloud_test2.py
+++ information-code-py/worldcloud_test2.py
@@ -1,14 +1,13 @@
 #coding=utf-8
-# from traceback import format_exception_only
-from turtle import width
+
 import jieba
-from wordcloud import WordCloud
+import wordcloud #导入词云模块
 # 处理图片模块
 import imageio
 mk = imageio.imread("download.jpg")
 w = wordcloud.WordCloud(mask=mk)
 
-w = worldcloud.WorldCloud(width=1000,
+w = wordcloud.WordCloud(width=1000,
                             height = 700,
                             background_color = 'white',
                             font_path = 'C:\Windows\Fonts\Sitka Banner\msyh.ttc',
@@ -16,5 +15,5 @@ w = worldcloud.WorldCloud(width=1000,
                             scale = 15)
 words = open('wyy.txt',encoding='gbk').read()
 new_words = ' '.join(jieba.cut(words))
-worldcloud.generate(new_words)
-wordcloud.to_file('wyy_pic.jpg')#保存到图片
+w.generate(new_words)
+w.to_file('wyy_pic.jpg')#保存到图片
